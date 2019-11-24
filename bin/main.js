a99493908a88b55a017ecac55b68dc71954e4a9d
Michael Zhang <michaelji520@gmail.com>
2019-11-24 17:14:29 +0800

    refactor

--- bin/main.js
+++ bin/main.js
@@ -9,11 +9,12 @@ const path = require('path');
 const util = require('util');
 const Gitlab = require('../lib/gitlab');
 const operation_options = require('../config/operation-options');
+const toolset = require('../lib/util');
 
 main();
 
 async function main() {
-  const config = await initProjectConfig();
+  const config = await initProjectConfig('gitlab.config.js');
   execGitlabCommand(config);
 }
 
@@ -28,15 +29,36 @@ async function execGitlabCommand(context) {
   switch (operation) {
     case 'merge':
       params = await inquirer.prompt(operation_options[operation]);
+      const spinner = ora('Launching merge request...');
+      spinner.start();
+      try {
+        const result = await gitlab.createMergeRequest(params);
+        spinner.succeed('Merge request sent!');
+        if (context.webhook) {
+          const { state, web_url, author } = result;
+          let data = {
+            'msgtype': 'markdown',
+            'markdown': {
+                'title': `${author.username} open the merge request from ${params.source_branch} to ${params.target_branch}`,
+                'text': `#### ${author.username} open the merge request from ${params.source_branch} to ${params.target_branch} \n` +
+                        `> Repository: ${context.project_name} \n\n` +
+                        `> Status: ${state} \n\n` +
+                        `> Title: [${params.title}](${web_url}) \n`
+            }
+          };
+          await util.sendWebhookMessage(webhook_address, data);
+        }
+      } catch(err) {
+        console.log(err);
+      }
       break;
     case 'branch':
       params = await inquirer.prompt(operation_options[operation]);
-      console.log(params)
       if (await gitlab.createRepositoryBranch(params)) {
-        const spinner = ora('正在拉取远程分支...');
+        const spinner = ora(`Fetching remote branch: ${params.branch}`);
         spinner.start();
         console.log((await execa(`git fetch origin ${params.branch}`)).stdout);
-        spinner.succeed('拉取远程分支成功!');
+        spinner.succeed(`Fetch remote branch ${params.branch} succeeded!`);
         console.log((await execa(`git checkout ${params.branch}`)).stdout);
       }
       break;
@@ -45,19 +67,21 @@ async function execGitlabCommand(context) {
 
 /**
  * @description initialize project info
+ * @param {string} config_file project config file name
  */
-async function initProjectConfig() {
+async function initProjectConfig(config_file) {
   const cwd = process.cwd();
-  const target = path.resolve(cwd, 'gitlab.config.js');
+  const target = path.resolve(cwd, config_file);
   let config = '';
   if (fs.existsSync(target)) {
     config = require(target);
   } else {
     let project = await inquirer.prompt(require('../config/project-options'));
     project.project_id = await queryProjectId(project);
-    const spinner = ora('正在将项目信息写入到配置文件...');
+    const spinner = ora(`Writing project config to local file: ${config_file}`);
     fs.writeFileSync(target, 'module.exports = ' + util.inspect(project, { depth: null }), 'utf-8');
-    spinner.succeed(`项目配置已写入到${target}中.`)
+    toolset.addFile2GitIgnore(config_file);
+    spinner.succeed(`Project config has been saved to ${target}.`)
     config = project;
   }
   return config;
@@ -71,7 +95,7 @@ async function initProjectConfig() {
 async function queryProjectId(context) {
   const {api, token, project_name} = context;
   let project_id = '';
-  const spinner = ora('正在获取项目ID...');
+  const spinner = ora('Querying project id...');
   spinner.start();
   const response = await axios.get(`${api}/projects?private_token=${token}&search=${project_name}`)
   const projects = response.data;
