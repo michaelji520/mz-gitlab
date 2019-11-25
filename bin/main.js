f6a390344784c3bcd60b0c7dfde293d96c30c82d
zhangji02 <zhangji02@zuoyebang.com>
2019-11-25 14:35:06 +0800

    bug fix

--- bin/main.js
+++ bin/main.js
@@ -49,7 +49,7 @@ async function execGitlabCommand(context) {
                         `> Title: [${params.title}](${web_url}) \n`
             }
           };
-          await util.sendWebhookMessage(webhook_address, data);
+          await util.sendWebhookMessage(context.webhook, data);
         }
       } catch(err) {
         console.log(err);
